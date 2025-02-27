48c97cbc3966bbdd58a9d9983839917bfacd7abc
GoloisaNinja <holidayninjastaff@gmail.com>
2025-02-27 01:11:33 -0500

    added checks for 7900xt - upped constraint

--- pageScraper.js
+++ pageScraper.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 const pageScraper = async (browser) => {
 	const url = 'https://www.newegg.com/p/pl?d=rtx+4070ti&Order=1';
-	let page = await browser.newPage();
-	console.log(`navigating to ${url}`);
-	await page.goto(url, { waitUntil: 'load' });
+	const xtUrl = "https://www.newegg.com/p/pl?d=7900+xt&Order=1"
+	let logheader = false;
+	
 	const elementExists = async (selector) => {
 		const element = await page.$(selector);
 		if (element) {
@@ -11,18 +11,21 @@ const pageScraper = async (browser) => {
 		}
 		return false;
 	};
-	await page.waitForSelector('.item-features', { visible: true });
-	await page.waitForSelector(
-		'div.item-action > ul.price > li.price-current > strong',
-		{ visible: true }
-	);
-	await page.waitForSelector('.item-title', { visible: true });
+	const waitForElements =  async(page) => {
+		await page.waitForSelector('.item-features', { visible: true });
+		await page.waitForSelector(
+			'div.item-action > ul.price > li.price-current > strong',
+			{ visible: true }
+		);
+		await page.waitForSelector('.item-title', { visible: true });
+	}
+	
 	const scrapeCards = async () => {
 		let cardHash = {};
 		const cells = await page.$$eval('.item-cell', (cell) => {
 			return cell.map((data) => {
 				let promo = data.querySelector('.item-promo');
-				let constraint = '650.00';
+				let constraint = '750.00';
 				let price = data
 					.querySelector('.price-current')
 					.textContent.match(/\d*,*\d+.\d{2}/g)[0]
@@ -44,37 +47,55 @@ const pageScraper = async (browser) => {
 		});
 		return cells;
 	};
-	const allCards = await scrapeCards();
-	const comparePrices = (a, b) => {
-		if (parseInt(a.price) > parseInt(b.price)) return 1;
-		if (parseInt(a.price) < parseInt(b.price)) return -1;
-		return 0;
-	};
-	let cards = allCards.filter((card) => card !== null);
-	cards.sort(comparePrices);
-	let data = 'Bot Run Date: ' + new Date().toString() + '\n\n';
-	if (cards.length) {
-		fs.writeFile('cards.txt', data, 'utf8', function (err) {
-			if (err) {
-				return console.log(err);
-			}
-		});
-	}
-	for (let i = 0; i < cards.length; i++) {
-		let subKeys = Object.keys(cards[i]);
-		let data = '';
-		for (let j = 0; j < subKeys.length; j++) {
-			let subKey = subKeys[j];
-			let lineEnd = '\n';
-			subKey === 'price' && (lineEnd = '\n\n');
-			data += cards[i][subKey] + lineEnd;
+
+
+	const processCards = (scrapedCards) => {
+		const comparePrices = (a, b) => {
+			if (parseInt(a.price) > parseInt(b.price)) return 1;
+			if (parseInt(a.price) < parseInt(b.price)) return -1;
+			return 0;
+		};
+		let cards = scrapedCards.filter((card) => card !== null);
+		cards.sort(comparePrices);
+		let data = 'Bot Run Date: ' + new Date().toString() + '\n\n';
+		if (cards.length && !logheader) {
+			logheader = true;
+			fs.writeFile('cards.txt', data, 'utf8', function (err) {
+				if (err) {
+					return console.log(err);
+				}
+			});
 		}
-		fs.appendFile('cards.txt', data, 'utf8', function (err) {
-			if (err) {
-				console.log(err);
+		for (let i = 0; i < cards.length; i++) {
+			let subKeys = Object.keys(cards[i]);
+			let data = '';
+			for (let j = 0; j < subKeys.length; j++) {
+				let subKey = subKeys[j];
+				let lineEnd = '\n';
+				subKey === 'price' && (lineEnd = '\n\n');
+				data += cards[i][subKey] + lineEnd;
 			}
-		});
+			fs.appendFile('cards.txt', data, 'utf8', function (err) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		}
 	}
+
+	let page = await browser.newPage();
+	console.log(`navigating to ${url}`);
+	await page.goto(url, { waitUntil: 'load' });
+	await waitForElements(page);
+	const allCards = await scrapeCards();
+	processCards(allCards);
+
+	await page.goto(xtUrl, { waitUntil: 'load' });
+	console.log(`navigating to ${xtUrl}`);
+	await waitForElements(page);
+	const xtCards = await scrapeCards();
+	processCards(xtCards);
+	
 	/* ADD TO CART STUFF
 	const hasProductBuyDiv = await elementExists('#ProductBuy');
 	if (hasProductBuyDiv) {
